291f21ab80ccf71d19ba901c28a2b4f82d26508c
Test <portalprfst@gmail.com>
2019-07-16 14:14:07 +0300

    rss для новостной ленты

--- classes/NewsDB.class.php
+++ classes/NewsDB.class.php
@@ -4,6 +4,9 @@ use \mysql_xdevapi\Exception;
 
 class NewsDB implements INewsDB{
     const DB_NAME = "db/news.db";
+    const RSS_NAME = "rss/rss.xml";
+    const RSS_TITLE = "Новостная лента";
+    const RSS_LINC = "http://news-ltst/index.php";
     private $_db = null;
 
     function __get($name)
@@ -69,7 +72,13 @@ class NewsDB implements INewsDB{
         $dt = time();
         $sql = "INSERT INTO msgs(title, category, description, source, datetime)
                     VALUES ('$title', '$category', '$description', '$source', '$dt')";
-        return $this->_db->exec($sql);
+        $res = $this->_db->exec($sql);
+        if (!$res)
+        {
+            return false;
+        }
+        $this->createRSS();
+        return true;
     }
 
     function db2Arr($data)
@@ -89,7 +98,10 @@ class NewsDB implements INewsDB{
                     WHERE category.id = msgs.category 
                     ORDER BY msgs.id DESC";
         $res = $this->_db->query($sql);
-        if (!$res) return false;
+        if (!$res)
+        {
+            return false;
+        }
         return $this->db2Arr($res);
     }
 
@@ -110,6 +122,63 @@ class NewsDB implements INewsDB{
     function clearInt($data)
     {
         return abs((int)$data);
+    }
+
+    function createRSS()
+    {
+        $dom = new DOMDocument("1.0", "utf-8");
+
+        $dom->formatOutput = true;
+        $dom->preserveWhiteSpace = false;
+
+        $rss = $dom->createElement("rss");
+        $dom->appendChild($rss);
+
+        $version = $dom->createAttribute("version");
+        $version->value = '2.0';
+        $rss->appendChild($version);
+
+        $channel = $dom->createElement("channel");
+
+        $title = $dom->createElement("title", self::RSS_TITLE);
+        $linc = $dom->createElement("linc", self::RSS_LINC);
+
+        $channel->appendChild($title);
+        $channel->appendChild($linc);
+
+        $rss->appendChild($channel);
+
+        $lenta = $this->getNews();
+        if (!$lenta)
+        {
+            return false;
+        }
+
+        foreach ($lenta as $news)
+        {
+            $item = $dom->createElement("item");
+            $title = $dom->createElement("title", $news["title"]);
+            $category = $dom->createElement("category", $news["category"]);
+
+            $desc = $dom->createElement("description");
+            $cdata = $dom->createCDATASection($news["description"]);
+            $desc->appendChild($cdata);
+
+            $linc = $dom->createElement("linc", "#");
+
+            $dt = date("r", $news["datetime"]);
+            $pubDate = $dom->createElement("pubDate", $dt);
+
+            $item->appendChild($title);
+            $item->appendChild($linc);
+            $item->appendChild($desc);
+            $item->appendChild($pubDate);
+            $item->appendChild($category);
+
+            $channel->appendChild($item);
+        }
+
+        $dom->save(self::RSS_NAME);
     }
 }
 ?>
